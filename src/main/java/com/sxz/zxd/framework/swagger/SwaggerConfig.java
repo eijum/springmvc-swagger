da0917d2b787bbb34455fd653ddca36610883d90
andrew <andrew@1858.com>
2017-07-27 22:02:48 +0800

    init project

---
+++ src/main/java/com/sxz/zxd/framework/swagger/SwaggerConfig.java
@@ -0,0 +1,17 @@
+package com.sxz.zxd.framework.swagger;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by dxjm on 2017/7/26.
+ */
+
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+
+
+}
